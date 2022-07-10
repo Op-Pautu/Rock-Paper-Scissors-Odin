2228308e149e6b3713a2d36e3ed5b24c60deddbb
Op Pautu <pautuop@gmail.com>
2022-07-10 13:11:42 +0530

    Add function to play game

--- paper.js
+++ paper.js
@@ -2,6 +2,7 @@ function computerChoice(){
     result = Math.floor(Math.random() * 3 + 1)
     return result;
 }
+let computerSelection = computerChoice();
 
 let playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
 let playerSelection;
@@ -20,5 +21,30 @@ switch(playerChoice) {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+    let playerScore = 0; 
+    let computerScore = 0; 
+    if (playerSelection == 1  && computerSelection == 3  ) {
+        playerScore += 1;
+        console.log(`Player Score is: ${playerScore}`)
+        return "Player wins";
+ }  else if (playerSelection == 3 && computerSelection == 1) {
+        computerScore += 1;
+        console.log(`Computer Score is: ${computerScore}`)
+        return "Computer wins";
+ }  else if (playerSelection > computerSelection) {
+        playerScore += 1;
+        console.log(`Player Score is: ${playerScore}`)
+        return "Player wins";
+  } else if (computerSelection > playerSelection) {
+        computerScore += 1;
+        console.log(`Computer Score is: ${computerScore}`)
+        return "Computer wins";
+ }  else if (playerSelection == computerSelection ) {
+        return "Draw";
+ }
 
+}
+
+console.log(playRound(playerSelection, computerSelection));
 
