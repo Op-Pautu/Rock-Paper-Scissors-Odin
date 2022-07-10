77e9983fb2a651d32dd1e998cd44ad3dd98601cd
Op Pautu <pautuop@gmail.com>
2022-07-10 14:10:21 +0530

    Edit and Add i terations to game

--- paper.js
+++ paper.js
@@ -2,43 +2,42 @@ function computerChoice(){
     result = Math.floor(Math.random() * 3 + 1)
     return result;
 }
-let computerSelection = computerChoice();
 
-let playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
-let playerSelection;
-switch(playerChoice) {
-    case "rock":
-        playerSelection =  1;
-        break;
-    case "paper":
-        playerSelection = 2;
-        break;
-    case "scissors":
-        playerSelection = 3;
-        break;
-    default: 
-        playerSelection = "Invalid. Choose rock, paper or scissors."
 
-}
+function playRound() {
+    let computerSelection = computerChoice();
 
-function playRound(playerSelection, computerSelection) {
-    let playerScore = 0; 
-    let computerScore = 0; 
+    let playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+    let playerSelection;
+    switch(playerChoice) {
+        case "rock":
+            playerSelection =  1;
+            break;
+        case "paper":
+            playerSelection = 2;
+            break;
+        case "scissors":
+            playerSelection = 3;
+            break;
+        default: 
+            playerSelection = "Invalid. Choose rock, paper or scissors."
+
+    }
+    
+
+
     if (playerSelection == 1  && computerSelection == 3  ) {
-        playerScore += 1;
-        console.log(`Player Score is: ${playerScore}`)
+        
+        
         return "Player wins";
  }  else if (playerSelection == 3 && computerSelection == 1) {
-        computerScore += 1;
-        console.log(`Computer Score is: ${computerScore}`)
+       
         return "Computer wins";
  }  else if (playerSelection > computerSelection) {
-        playerScore += 1;
-        console.log(`Player Score is: ${playerScore}`)
+       
         return "Player wins";
   } else if (computerSelection > playerSelection) {
-        computerScore += 1;
-        console.log(`Computer Score is: ${computerScore}`)
+        
         return "Computer wins";
  }  else if (playerSelection == computerSelection ) {
         return "Draw";
@@ -46,5 +45,46 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound());
 
+function game() {
+    let playerScore = 0; 
+    let computerScore = 0;
+   
+            
+    for (let i = 1;  playerScore <= 6 ||  computerScore <= 6; i++) { 
+        
+        let round = playRound()
+        if (round == "Player wins") {
+            console.log(`Player Score: ${++playerScore}`)
+        }
+        else if (round == "Computer wins") {
+            console.log(`Computer Score: ${++computerScore}`)
+        }
+        else {
+            console.log("Stalemate") 
+        }
+        
+        if (playerScore === 5) {
+            console.log("Player has won the match")
+            alert("You did it! You beat the computer! You win nothing!")
+                if (confirm("Do you want to play again?") === true) {
+                    game()
+                } else {
+                    break;
+                 }
+        } else if (computerScore === 5) {
+            console.log("Player has lost the match")
+            alert("You are, above all else, a failure to yourself and to mankind. How do you lose to a computer?")
+            if (confirm("Do you want to play again?") === true) {
+                game() 
+            } else {
+            break;
+            }
+        } else {
+            alert("The score is " + playerScore + " to " + computerScore + " right now. First to 5 wins!" )
+        }
+    }
+  } 
+    
+game()
